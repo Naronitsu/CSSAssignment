3693215cf52948187bb1eff39e9dda94e8aab55b
Damian Navarro <damiannav03@gmail.com>
2025-04-07 11:21:07 +0200

    components/services

---
+++ src/app/list-appointment/list-appointment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-appointment',
+  imports: [],
+  templateUrl: './list-appointment.component.html',
+  styleUrl: './list-appointment.component.css'
+})
+export class ListAppointmentComponent {
+
+}
